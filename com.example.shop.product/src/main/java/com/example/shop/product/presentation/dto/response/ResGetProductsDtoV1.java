57efabd54b16ee2dc573b8222ff163dffbe8e744
jaybon1 <ponggas2@gmail.com>
2025-10-28 14:52:40 +0900

    Update ResGetProductsDtoV1.java

--- com.example.shop.product/src/main/java/com/example/shop/product/presentation/dto/response/ResGetProductsDtoV1.java
+++ com.example.shop.product/src/main/java/com/example/shop/product/presentation/dto/response/ResGetProductsDtoV1.java
@@ -1,10 +1,14 @@
 package com.example.shop.product.presentation.dto.response;
 
+import com.example.shop.product.domain.model.Product;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.web.PagedModel;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,12 +16,32 @@ public class ResGetProductsDtoV1 {
 
     private final ProductPageDto productPage;
 
+    public static ResGetProductsDtoV1 of(Page<Product> productPage) {
+        return ResGetProductsDtoV1.builder()
+                .productPage(new ProductPageDto(productPage))
+                .build();
+    }
+
     @Getter
     @ToString
     public static class ProductPageDto extends PagedModel<ProductPageDto.ProductDto> {
 
-        public ProductPageDto(Page<ProductDto> page) {
-            super(page);
+        public ProductPageDto(Page<Product> productPage) {
+            super(
+                    new PageImpl<>(
+                            ProductDto.from(productPage.getContent()),
+                            productPage.getPageable(),
+                            productPage.getTotalElements()
+                    )
+            );
+        }
+
+        public ProductPageDto(ProductDto... productDtoArray) {
+            super(
+                    new PageImpl<>(
+                            List.of(productDtoArray)
+                    )
+            );
         }
 
         @Getter
@@ -28,8 +52,20 @@ public class ResGetProductsDtoV1 {
             private final Long price;
             private final Long stock;
 
+            private static List<ProductDto> from(List<Product> productList) {
+                return productList.stream()
+                        .map(ProductDto::from)
+                        .toList();
+            }
+
+            public static ProductDto from(Product product) {
+                return ProductDto.builder()
+                        .id(product.getId().toString())
+                        .name(product.getName())
+                        .price(product.getPrice())
+                        .stock(product.getStock())
+                        .build();
+            }
         }
-
     }
-
 }
